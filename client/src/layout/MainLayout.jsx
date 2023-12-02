110c63b9fbc391172b65ace21fa1dea166bedb5f
masud-rana44 <mdmasudrana4488@gmail.com>
2023-12-03 01:22:07 +0600

    Implemented hero section

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <main className="pt-[72px] lg:pt-[88px] min-h-[calc(100vh-312px)]">
+      <main className="pt-[72px] lg:pt-[80px] min-h-[calc(100vh-312px)]">
         <Outlet />
       </main>
       <Footer />
