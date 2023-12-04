38e1d63751581ddc9f72d961284b0cf6776fb1c6
masud-rana44 <mdmasudrana4488@gmail.com>
2023-12-05 01:30:34 +0600

    Updated leaderboard

--- client/src/pages/LeaderBoard.jsx
+++ client/src/pages/LeaderBoard.jsx
@@ -37,7 +37,7 @@ const LeaderBoard = () => {
                   </div>
                 </div>
               </div>
-              <div className="font-medium">{item?.totalPrizeMoney}</div>
+              <div className="font-medium">${item?.totalPrizeMoney}</div>
             </div>
           ))}
         </div>
