f5774986777446e33ce6aa3af8098b1c7ae58864
masud-rana44 <mdmasudrana4488@gmail.com>
2023-12-05 01:45:43 +0600

    Update our winners section

--- client/src/pages/home/Winners.jsx
+++ client/src/pages/home/Winners.jsx
@@ -14,19 +14,23 @@ const Winners = () => {
         <SectionHeading>Our Winners</SectionHeading>
         <div className="mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
           {contests?.map((contest) => (
-            <div key={contest._id} className="bg-gray-100 rounded-lg p-4">
+            <div key={contest._id} className="relative bg-gray-100 rounded-lg">
               <div className="flex items-center justify-between">
-                <div className="flex items-center">
+                <div className="absolute inset-0 bg-gray-900 bg-opacity-70"></div>
+                <div className="absolute top-10 left-[10%] flex flex-col items-center text-center">
+                  <h2 className="text-xl text-white text-center font-semibold p-1">
+                    {contest?.title}
+                  </h2>
                   <img
                     src={contest?.winnerInfo?.image}
                     alt="avatar"
-                    className="w-12 h-12 rounded-full"
+                    className="w-12 h-12 rounded-full mx-auto text-center"
                   />
-                  <div className="ml-4">
+                  <div className="ml-4  text-white text-center">
                     <h1 className="text-xl font-bold">
                       {contest?.winnerInfo?.name}
                     </h1>
-                    <p className="text-gray-500">
+                    <p className="text-gray-400 font-medium">
                       {contest?.winnerInfo?.email}
                     </p>
                   </div>
@@ -36,10 +40,9 @@ const Winners = () => {
                 <img
                   src={contest?.image}
                   alt="contest"
-                  className="w-full h-[200px] object-cover"
+                  className="w-full h-[260px] object-cover"
                 />
               </div>
-              <h2 className="text-xl font-semibold p-1">{contest?.title}</h2>
             </div>
           ))}
         </div>
